d7c6ff99b282817d3feb14731387993c7e71ac28
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-09-26 22:33:09 +0300

    add README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+## Purpose of the Project
+This project's purpose is to transform data provided in an excel document. 
+
+## Expected input and output
+- The expected input is an excel document with a sheet that contains that needs to be transformed.
+- The output is an excel document with transformed data.
+
+## Installation steps
+- Ensure Python 3.10 is installed on your computer.
+- Clone the repo https://github.com/manpikingillz/newpage_assigmnment.git
+- Changed directory to `newpage_assigmnment` i.e `cd newpage_assigmnment`
+- Confirm that python 3.10 is installed by executing `python3.10 --version`
+- Create virtual environment `python3.10 -m venv venv`
+- Install project dependencies - `pip install -r requirements.txt`
+- To run the app, execute the command `python run.py`
+- To run the tests, execute - `pytest`
+## Use cases and edge cases covered in the code.
+## Known limitations
+## Assumptions made
--- app/test_sites_data.py
+++ app/test_sites_data.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 import pytest
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 import io
+import mock
 
 from sites_data import SitesData
 
 
 @pytest.fixture
 def sites_data():
-    '''Returns a SitesDat instance with dataframe'''
     date1 = datetime.strptime('2021-01-01', '%Y-%m-%d')
     date2 = datetime.strptime('2021-01-02', '%Y-%m-%d')
 
@@ -81,32 +81,31 @@ def sites_data():
 
 
 def test_number_of_sites(sites_data):
+    # Test case to test that number of sites is correct'
     sites_data.number_of_sites() == 3
 
 
 def test_days_count_between_dates(sites_data):
+    # Test case to test days count between dates
     start_date = sites_data.dataframe.iloc[0,0]
     end_date = sites_data.dataframe.iloc[1,0]
     delta = end_date - start_date
     assert delta.days == 1
 
-# def test_transform_data(sites_data):
-#     date1 = datetime.strptime('2021-01-01', '%Y-%m-%d')
-#     date2 = datetime.strptime('2021-01-02', '%Y-%m-%d')
-#     transformed_data = pd.DataFrame({
-#                 'Day of Month': [1,2,1,2,1,2],
-#                 # 'Date': [date1, date2, date1, date2, date1, date2],
-#                 'Site ID': ['Site 1', 'Site 1', 'Site 2', 'Site 2', 'Site 3', 'Site 3'],
-#                 # 'Page Views': [6,4,4,5,4,4],
-#                 'Unique Visitors': [4,2,2,3,2,2],
-#                 'Total Time Spent': [11,14,5,6,8,8],
-#                 'Visits': [4,2,2,3,2,2],
-#                 'Average Time Spent on Site': [0.1,0.1,0,0.1,0.1,0.1]
-#             })
-#     transformed_data.to_excel('intest.xlsx')
-#     sites_data.transform_data().to_excel('from_method.xlsx')
-#     assert_frame_equal(transformed_data.reset_index(drop=True), sites_data.transform_data().reset_index(drop=True))
-#     # transformed_data.compare(sites_data.transform_data())
-# # def test_convert_dataframe_to_excel(sites_data, output_file):
-# #     transformed_data = sites_data.transform_data()
-# #     transformed_data.to_excel(output_file, index=False)
+def test_transform_data(sites_data):
+    # Test case to test that data will be transformed correctly.
+    date1 = '2021-01-01'
+    date2 = '2021-01-02'
+
+    expected_transformed_dataframe = pd.DataFrame({
+                'Day of Month': [1,2,1,2,1,2],
+                'Date': [date1, date2, date1, date2, date1, date2],
+                'Site ID': ['Site 1', 'Site 1', 'Site 2', 'Site 2', 'Site 3', 'Site 3'],
+                'Page Views': ['6','4','4','5','4','4'],
+                'Unique Visitors': ['4','2','2','3','2','2'],
+                'Total Time Spent': ['11', '14', '5', '6', '8', '8'],
+                'Visits': ['4','2','2','3','2','2'],
+                'Average Time Spent on Site': ['0.1','0.1','0','0.1','0.1','0.1']
+            })
+
+    assert_frame_equal(expected_transformed_dataframe.reset_index(drop=True), sites_data.transform_data().reset_index(drop=True))
